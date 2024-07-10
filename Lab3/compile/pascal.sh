1390d9e49213a7bac0a035d220fe0ecb17c1c796
Logan MacDougall <lg463300@dal.ca>
2024-07-10 14:37:05 -0300

    removed user specific scripts and renamed some folders

--- Lab3/compile/pascal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ~/Courses/CSCI2122/Labs/Lab3/macdougall
-
-cp CI/objects/pascal/* Lab3/pascal/
-cp -R Lab3/lib Lab3/pascal/
-cd Lab3/pascal/
-gcc --std=c18 -lm -Ilib lib/mmath.c lib/mstring.c pascal.c pascalM.o -o pascal
-./pascal > output
